a83a8ff731c1be27a46577d6961a6d22b8b92d02
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2022-10-17 21:22:23 -0300

    TEST: Testing a new fractal

--- fractal/main.c
+++ fractal/main.c
@@ -1,191 +1,114 @@
-#include "errnomsg.h"
-#include "mandelbrot.h"
-#include "types.h"
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
+#include <stdint.h>
 
-void output_image_header(FILE *output_file, int image_width, int image_height) {
-  fprintf(output_file, "P6 %d %d 255 ", image_width, image_height);
+int generate_fractal(
+    char x_min[20],
+    char x_max[20],
+    char y_min[20],
+    char y_max[20],
+    char max_iter[20],
+    char x_res[20],
+    char file_name[20]);
+
+int main()
+{
+  generate_fractal(
+      "0.27085", "0.27100", "0.004640", "0.004810", "1000", "1024",
+      "pic.ppm");
 }
 
-int main(int argc, char **argv) {
-  if (argc <= 1) {
-    printf("Usage: %s [options] output-file\n"
-           "\n"
-           "Options:\n"
-           "    -w, --width        Image width; default is 1200\n"
-           "    -h, --height       Image height; default is 800\n"
-           "    -x, --x-range      Range to map pixels to; default is -2,1\n"
-           "    -y, --y-range      Range to map pixels to; default is -1,1\n"
-           "    -i, --iterations   Number of iterations to check; default is "
-           "1000\n"
-           "    -c, --center       Specify a center.\n"
-           "\n"
-           "When a center is specified, the x and y ranges\n"
-           "will become relative to the center. If only one\n"
-           "range is specified, the other will be adjusted to\n"
-           "match the aspect ratio of the image.\n"
-           "\n"
-           "Example usage: ./mandelbrot-gen --center -.745,.13 -x -.008,.008 "
-           "-w 5464 -h 3072 out.ppm\n"
-           "Another example identical to the former: ./mandelbrot-gen -x "
-           "-.753,-.737 -y .12550219619326503,.13449780380673498 -w 5464 -h "
-           "3072 out.ppm\n"
-           "No space is allowed before nor after the comma\n"
-           "\n"
-           "When output-file is -, output is to stdout\n",
-           argv[0]);
-    exit(0);
-  }
+int generate_fractal(
+    char *x_min,
+    char *x_max,
+    char *y_min,
+    char *y_max,
+    char *max_iter,
+    char *x_res,
+    char *file_name)
+{
+  {
+    /* Parse the command line arguments. */
+    // if (argc != 8) {
+    //   printf("Usage:   %s <xmin> <xmax> <ymin> <ymax> <maxiter> <xres> <out.ppm>\n", argv[0]);
+    //   printf("Example: %s 0.27085 0.27100 0.004640 0.004810 1000 1024 pic.ppm\n", argv[0]);
+    //   exit(EXIT_FAILURE);
+    // }
 
-  char *output_filename = NULL;
-  int image_width = 1200;
-  int image_height = 800;
-  long iterations = 1000;
-  double xmin = 0;
-  double xmax = 0;
-  double ymin = 0;
-  double ymax = 0;
-  double centerx = 0;
-  double centery = 0;
+    /* The window in the plane. */
+    const double xmin = atof(x_min);
+    const double xmax = atof(x_max);
+    const double ymin = atof(y_min);
+    const double ymax = atof(y_max);
 
-  bool ddash = false; /* the double dash "--" */
-  bool center = false;
-  int i;
-  for (i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--") == 0) {
-      if (!ddash)
-        ddash = true;
-      else
-        output_filename = "--";
+    /* Maximum number of iterations, at most 65535. */
+    const uint16_t maxiter = (unsigned short)atoi(max_iter);
 
-    } else if (argv[i][0] == '-' && argv[i][1] == 0) {
-      output_filename = argv[i];
+    /* Image size, width is given, height is computed. */
+    const int xres = atoi(x_res);
+    const int yres = (xres * (ymax - ymin)) / (xmax - xmin);
 
-    } else if (argv[i][0] == '-' && !ddash) {
+    /* The output file name */
+    const char *filename = file_name;
 
-#define options(x, y) if (strcmp(argv[i], x) == 0 || strcmp(argv[i], y) == 0)
-#define param_chk()                                                            \
-  if (i + 1 == argc) {                                                         \
-    printf("Missing parameter for '%s'\n", argv[i]);                           \
-    exit(1);                                                                   \
-  }
+    /* Open the file and write the header. */
+    FILE *fp = fopen(filename, "wb");
+    char *comment = "# Mandelbrot set"; /* comment should start with # */
 
-      options("-w", "--width") {
-        param_chk();
-        sscanf(argv[++i], "%d", &image_width);
-      }
-      else options("-h", "--height") {
-        param_chk();
-        sscanf(argv[++i], "%d", &image_height);
-      }
-      else options("-i", "--iterations") {
-        param_chk();
-        sscanf(argv[++i], "%ld", &iterations);
-      }
-      else options("-x", "--x-range") {
-        param_chk();
-        if (sscanf(argv[++i], "%lf,%lf", &xmin, &xmax) < 2) {
-          fprintf(stderr, "Bad format for parameter '%s'\n", argv[i - 1]);
-          return 1;
-        } else if (xmin == xmax) {
-          fprintf(stderr, "Invalid range for parameter '%s'\n", argv[i - 1]);
-          return 1;
+    /*write ASCII header to the file*/
+    fprintf(fp,
+            "P6\n# Mandelbrot, xmin=%lf, xmax=%lf, ymin=%lf, ymax=%lf, maxiter=%d\n%d\n%d\n%d\n",
+            xmin, xmax, ymin, ymax, maxiter, xres, yres, (maxiter < 256 ? 256 : maxiter));
+
+    /* Precompute pixel width and height. */
+    double dx = (xmax - xmin) / xres;
+    double dy = (ymax - ymin) / yres;
+
+    double x, y; /* Coordinates of the current point in the complex plane. */
+    double u, v; /* Coordinates of the iterated point. */
+    int i, j;    /* Pixel counters */
+    int k;       /* Iteration counter */
+    for (j = 0; j < yres; j++)
+    {
+      y = ymax - j * dy;
+      for (i = 0; i < xres; i++)
+      {
+        double u = 0.0;
+        double v = 0.0;
+        double u2 = u * u;
+        double v2 = v * v;
+        x = xmin + i * dx;
+        /* iterate the point */
+        for (k = 1; k < maxiter && (u2 + v2 < 4.0); k++)
+        {
+          v = 2 * u * v + y;
+          u = u2 - v2 + x;
+          u2 = u * u;
+          v2 = v * v;
+        };
+        /* compute  pixel color and write it to file */
+        if (k >= maxiter)
+        {
+          /* interior */
+          const unsigned char black[] = {0, 0, 0, 0, 0, 0};
+          fwrite(black, 6, 1, fp);
         }
+        else
+        {
+          /* exterior */
+          unsigned char color[6];
+          color[0] = k >> 8;
+          color[1] = k & 255;
+          color[2] = k >> 8;
+          color[3] = k & 255;
+          color[4] = k >> 8;
+          color[5] = k & 255;
+          fwrite(color, 6, 1, fp);
+        };
       }
-      else options("-y", "--y-range") {
-        param_chk();
-        if (sscanf(argv[++i], "%lf,%lf", &ymin, &ymax) < 2) {
-          fprintf(stderr, "Bad format for parameter '%s\n", argv[i - 1]);
-          return 1;
-        } else if (ymin == ymax) {
-          fprintf(stderr, "Invalid range for parameter '%s'\n", argv[i - 1]);
-          return 1;
-        }
-      }
-      else options("-c", "--center") {
-        param_chk();
-        if (sscanf(argv[++i], "%lf,%lf", &centerx, &centery) < 2) {
-          fprintf(stderr, "Bad format for parameter '%s\n", argv[i - 1]);
-          return 1;
-        }
-        center = true;
-      }
-      else {
-        printf("Unrecognized option '%s'\n", argv[i]);
-        exit(1);
-      }
-
-#undef options
-#undef param_chk
-
-    } else {
-      output_filename = argv[i];
     }
+    fclose(fp);
+    return 0;
   }
-
-  if (output_filename == NULL) {
-    fprintf(stderr, "No output file specified\n");
-    return 1;
-  }
-
-  if (center) {
-    if ((xmin || xmax) && !(ymin || ymax)) {
-      /* auto adjust y range according to image size */
-      double y = (double)image_height / (double)image_width * (xmax - xmin);
-      xmin += centerx;
-      xmax += centerx;
-
-      ymin = centery - y / 2;
-      ymax = centery + y / 2;
-
-    } else if ((ymin || ymax) && !(xmin || xmax)) {
-      /* auto adjust x range according to image size */
-      double x = (double)image_width / (double)image_height * (ymax - ymin);
-      ymin += centery;
-      ymax += centery;
-
-      xmin = centerx - x / 2;
-      xmax = centerx + x / 2;
-    } else if ((ymin || ymax) && (xmin || xmax)) {
-      fprintf(stderr, "Only specify one range when using '-c' or '--center'\n");
-      return 1;
-    } else {
-      fprintf(stderr, "No range specified, ignoring '--center' option\n");
-      ymin = -1;
-      ymax = 1;
-      xmin = -2;
-      xmax = 1;
-    }
-  } else {
-    if (!(xmin || xmax)) {
-      xmin = -2;
-      xmax = 1;
-    }
-
-    if (!(ymin || ymax)) {
-      ymin = -1;
-      ymax = 1;
-    }
-  }
-
-  FILE *output_file;
-
-  if (output_filename[0] == '-' && output_filename[1] == 0) {
-    output_file = stdout;
-  } else {
-    output_file = fopen(output_filename, "w");
-    if (output_file == NULL) {
-      errno_msg(output_filename);
-      return 1;
-    }
-  }
-
-  output_image_header(output_file, image_width, image_height);
-
-  generate(output_file, image_width, image_height, xmin, xmax, ymin, ymax, 256,
-           iterations);
-  return 0;
 }
